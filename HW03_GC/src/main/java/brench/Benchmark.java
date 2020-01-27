d9bd2c7f7a44ac89dd91324197064d35873456d4
Nikita <nicromanov3000@gmail.com>
2020-01-27 23:07:20 +0300

    try 1

---
+++ HW03_GC/src/main/java/brench/Benchmark.java
@@ -0,0 +1,40 @@
+package brench;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Benchmark implements BenchmarkMBean {
+    private final int loopCounter;
+    private volatile int size = 0; //volatile  Результат операции записи
+    // значения в volatile переменную одним потоком, становится виден всем
+    // другим потокам, которые используют эту переменную для чтения из нее значения.
+
+    public Benchmark(int loopCounter) {
+        this.loopCounter = loopCounter;
+    }
+
+    void run() throws InterruptedException {
+        for(int i=0; i<loopCounter; i++){
+            int localSize=size;
+            List<Integer> testList = new ArrayList<>();
+
+            for(int j=0; j<localSize; j++);
+            testList.add(new Integer(9));
+            if(i%2==0){
+                testList.remove(i);
+            }
+
+            Thread.sleep(10);
+        }
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public void setSize(int size) {
+        this.size=size;
+    }
+}
---
+++ HW03_GC/src/main/java/brench/TestGC.java
@@ -0,0 +1,58 @@
+package brench;
+
+import com.sun.management.GarbageCollectionNotificationInfo;
+
+import javax.management.MBeanServer;
+import javax.management.NotificationEmitter;
+import javax.management.NotificationListener;
+import javax.management.ObjectName;
+import javax.management.openmbean.CompositeData;
+import java.lang.management.GarbageCollectorMXBean;
+import java.lang.management.ManagementFactory;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestGC {
+    public static void main(String... args) throws Exception {
+        System.out.println( "Starting pid: " + ManagementFactory.getRuntimeMXBean().getName() );
+
+        int size = 9 *1000*1000;
+        int loopCounter=1000;
+
+        MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+        ObjectName oName = new ObjectName("brench.Benchmark");
+
+        Benchmark mbean = new Benchmark(loopCounter);
+        mbs.registerMBean(mbean,oName);
+        mbean.setSize(size);
+        mbean.run();
+
+    }
+
+
+    //Для логов взял метод из вебинара
+    private static void goToMonitor(){
+        List<GarbageCollectorMXBean> gcbeans = java.lang.management.ManagementFactory.getGarbageCollectorMXBeans();
+        for ( GarbageCollectorMXBean gcbean : gcbeans ) {
+            System.out.println( "GC name:" + gcbean.getName() );
+            NotificationEmitter emitter = (NotificationEmitter) gcbean;
+            NotificationListener listener = (notification, handback ) -> {
+                if ( notification.getType().equals( GarbageCollectionNotificationInfo.GARBAGE_COLLECTION_NOTIFICATION ) ) {
+                    GarbageCollectionNotificationInfo info = GarbageCollectionNotificationInfo.from( (CompositeData) notification.getUserData() );
+                    String gcName = info.getGcName();
+                    String gcAction = info.getGcAction();
+                    String gcCause = info.getGcCause();
+
+                    long startTime = info.getGcInfo().getStartTime();
+                    long duration = info.getGcInfo().getDuration();
+
+                    System.out.println( "start:" + startTime + " Name:" + gcName + ", action:" + gcAction + ", gcCause:" + gcCause + "(" + duration + " ms)" );
+                }
+            };
+            emitter.addNotificationListener( listener, null, null );
+        }
+    }
+
+}
+
+
