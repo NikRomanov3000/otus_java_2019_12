c082a4fff2bcd1adb8415c5fd75ac7d27d1842a9
N.Romanov <nicromanov3000@gmail.com>
2020-03-02 13:28:37 +0300

    try1

--- HW03-Logger/src/main/java/MyIoC.java
+++ HW03-Logger/src/main/java/MyIoC.java
@@ -1,4 +1,3 @@
-import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
@@ -17,34 +16,40 @@ public class MyIoC {
         private final TestLoggingInterface myClass;
         private Map<Method, Boolean> helpMethodsMap = new HashMap<>();
 
+
         MyInvocationHandler(TestLoggingInterface myClass) {
             this.myClass = myClass;
             Class<?> myClassInterface = myClass.getClass().getInterfaces()[0];
             Method[] methods = myClass.getClass().getMethods();
-           int count = myClass.getClass().getInterfaces()[0].getMethods().length;
-            System.out.println(count);
-            System.out.println(myClass.getClass().toString());
-            System.out.println(myClass.getClass().getInterfaces()[0].toString());
 
-                for (Method method : methods) {
-                    if (method.isAnnotationPresent(Log.class))
-                        helpMethodsMap.put(method, true);
-                    else
-                        helpMethodsMap.put(method, false);
+            for (Method method : methods) {
+                if (method.isAnnotationPresent(Log.class)) {
+                    String annotationMethodName;
+                    annotationMethodName = method.getName();
+                    initHelpMap(myClass, annotationMethodName);
                 }
+            }
+        }
+
+        private void initHelpMap(TestLoggingInterface myClass, String annotationMethodName) {
+            Method[] methodsInterface = myClass.getClass().getInterfaces()[0].getMethods();
+            for (Method method : methodsInterface) {
+                if (method.getName() == annotationMethodName)
+                    helpMethodsMap.put(method, true);
+            }
+
         }
 
         @Override //Map канает внутри invoke
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-            System.out.println(method);
-            // System.out.println(helpMap.get(method));
-            //if (helpMethodsMap.get(method)) {
-                if (args != null)
-                    System.out.println("invoking method: " + method.getName() + "with param: " + args[0].toString());
-                else
+            if (helpMethodsMap.get(method)) {
+                if (args != null) {
+                    System.out.print("invoking method: " + method.getName() + "with param-s: ");
+                    for (int i = 0; i < args.length; i++)
+                        System.out.println(args[i]);
+                } else
                     System.out.println("invoking method: " + method.getName());
-            //}
-
+            }
 
             return method.invoke(myClass, args);
         }
