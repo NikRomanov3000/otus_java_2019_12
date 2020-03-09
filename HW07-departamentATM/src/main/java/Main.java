ef8eb41bf9828345f4b22e97224bc6496492a86b
Nikita <nicromanov3000@gmail.com>
2020-03-09 15:34:39 +0300

    try1

--- HW07-departamentATM/src/main/java/Main.java
+++ HW07-departamentATM/src/main/java/Main.java
@@ -7,10 +7,11 @@ import DepartmentPatrens.Observer.EventRefreshAll;
 public class Main {
     public static void main(String[] args) {
         Facade_DepartamentATM departamentATM = new Facade_DepartamentATM();
+        System.out.println(departamentATM.getBalance()); //начальный баланс
         departamentATM.getATM().getBanknotesFromATM(99900);
-        System.out.println(departamentATM.getBalance());
+        System.out.println(departamentATM.getBalance()); //баланс после снятия
         departamentATM.RefreshAll();
-        System.out.println(departamentATM.getBalance());
+        System.out.println(departamentATM.getBalance()); // баланс после рефреша
         departamentATM.showAllATM();
     }
 }
