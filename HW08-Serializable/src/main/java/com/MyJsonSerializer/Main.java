c758c8d981910b6e15bce4ecb02ad67540033570
N.Romanov <nicromanov3000@gmail.com>
2020-03-16 12:35:49 +0300

    Beta version. Need to fix all primitive types are String

--- HW08-Serializable/src/main/java/com/MyJsonSerializer/Main.java
+++ HW08-Serializable/src/main/java/com/MyJsonSerializer/Main.java
@@ -1,10 +1,6 @@
 package com.MyJsonSerializer;
-import com.MyJsonSerializer.ClassForTest.SimpleUser;
 import com.MyJsonSerializer.ClassForTest.User;
 import com.google.gson.Gson;
-
-import java.lang.reflect.Field;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IllegalAccessException, NoSuchFieldException {
@@ -14,8 +10,8 @@ public class Main {
 
         String gsonResult = gson.toJson(user);
         String myResult = nikson.toJson(user);
-        System.out.println(gsonResult);
-        System.out.println(myResult);
+        System.out.println("Gson result:   " + gsonResult);
+        System.out.println("myJson result: "+ myResult);
         System.out.println("Test status: "+ gsonResult.equals(myResult));
 
     }
