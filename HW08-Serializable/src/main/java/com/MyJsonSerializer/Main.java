6e825b05ff7e57fbe4f1165bfba7a1ea34e0ffc1
N.Romanov <nicromanov3000@gmail.com>
2020-03-16 12:18:41 +0300

    Beta version. Need to fix all primitive types are String

--- HW08-Serializable/src/main/java/com/MyJsonSerializer/Main.java
+++ HW08-Serializable/src/main/java/com/MyJsonSerializer/Main.java
@@ -10,26 +10,13 @@ public class Main {
     public static void main(String[] args) throws IllegalAccessException, NoSuchFieldException {
         Gson gson = new Gson();
         User user = new User();
-        int testInt = 100;
         NikSON nikson = new NikSON();
-        SimpleUser simpleUser = new SimpleUser("Fox", 22);
-        List<String> hobbyList = new ArrayList<>(Arrays.asList("Board games", "Metal music", "YouTube"));
-        Set<String> stackOfInterest = new HashSet<>(Arrays.asList("Java", "Spring boot", "Hibernate", "Docker", "Postgres"));
-
-
-        Field[] fields = user.getClass().getDeclaredFields();
-        for(Field field:fields){
-            field.setAccessible(true);
-            if(field.get(user)!=null) {
-              //  System.out.println(field.getName() +" "+ field.getType() + " " + Iterable.class.isAssignableFrom(field.getType()));
-            }
-        }
-      //  System.out.println(user.getClass().getField("hobbyList").getName());
 
         String gsonResult = gson.toJson(user);
         String myResult = nikson.toJson(user);
         System.out.println(gsonResult);
         System.out.println(myResult);
+        System.out.println("Test status: "+ gsonResult.equals(myResult));
 
     }
 }
--- HW08-Serializable/src/main/java/com/MyJsonSerializer/NikSON.java
+++ HW08-Serializable/src/main/java/com/MyJsonSerializer/NikSON.java
@@ -3,13 +3,7 @@ package com.MyJsonSerializer;
 import javax.json.*;
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
-import java.lang.reflect.Type;
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 
 
 public class NikSON {
@@ -25,20 +19,26 @@ public class NikSON {
             field.setAccessible(true);
             if (field.get(obj) != null) {
                 if (field.getType().isPrimitive()) { // for Primitive
-                    //нужна проверка на числа и булен
                     result.add(field.getName(), String.valueOf(field.get(obj)));
                 }
 
                 if (field.getType().isArray()) { //for Array
+
                     var jsonArray = Json.createArrayBuilder();
                     int length = Array.getLength(field.get(obj));
                     Object[] array = new Object[length];
+
                     for (int i = 0; i < length; i++) {
                         array[i] = Array.get(field.get(obj), i);
-                        //--System.out.println(array[i].getClass());
-                        //jsonArray.add(String.valueOf(array[i]));
-                        //add(field.getName(), createJSON(field.get(obj)));
-                        jsonArray.add(createJSON(field.get(obj)));
+                        if (checkPrimitiveClass(array[i].getClass())) {
+                            jsonArray.add(String.valueOf(array[i]));
+                        } else {
+                            if (String.class.isAssignableFrom(field.getType())) {
+                                result.add(field.getName(), field.get(obj).toString());
+                            } else {
+                                jsonArray.add(createJSON(array[i]));
+                            }
+                        }
                     }
                     result.add(field.getName(), jsonArray.build());
                 }
@@ -47,6 +47,7 @@ public class NikSON {
                     var jsonCollections = Json.createArrayBuilder((Collection<?>) field.get(obj));
                     result.add(field.getName(), jsonCollections.build());
                 }
+                //for Objects
                 if (!field.getType().isPrimitive() && !Iterable.class.isAssignableFrom(field.getType()) && !field.getType().isArray()) {
                     if (String.class.isAssignableFrom(field.getType())) {
                         result.add(field.getName(), field.get(obj).toString());
@@ -54,13 +55,25 @@ public class NikSON {
                         result.add(field.getName(), createJSON(field.get(obj)));
                     }
                 }
-
             }
-
         }
         return result.build();
     }
+
+    private boolean checkPrimitiveClass(Object object) {
+        if (Integer.class.isAssignableFrom((Class<?>) object)
+                || Short.class.isAssignableFrom((Class<?>) object)
+                || Float.class.isAssignableFrom((Class<?>) object)
+                || Byte.class.isAssignableFrom((Class<?>) object)
+                || Double.class.isAssignableFrom((Class<?>) object)
+                || Long.class.isAssignableFrom((Class<?>) object)
+                || Character.class.isAssignableFrom((Class<?>) object)
+                || Boolean.class.isAssignableFrom((Class<?>) object)) {
+            return true;
+        } else return false;
+    }
 }
+
 
 
 
